805d24189bb148e64f80fba14396a084a385ee27
Rohan McLure <rmclure@linux.ibm.com>
2023-05-08 13:29:21 +1000

    nvim: Add custom snippets capability

--- nvim/lua/lsp/init.lua
+++ nvim/lua/lsp/init.lua
@@ -1,7 +1,7 @@
 local lsp = require('lsp-zero').preset({
   name = 'minimal',
   set_lsp_keymaps = false,
-  manage_nvim_cmp = true,
+  manage_nvim_cmp = 'recommended',
   suggest_lsp_servers = true,
 })
 
