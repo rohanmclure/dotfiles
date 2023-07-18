7a6a9073881c469f682d7604a9abeaa2de33ce13
Rohan McLure <rmclure@linux.ibm.com>
2023-07-18 13:36:54 +1000

    Revert "nvim: Why did I set default tabstop?"
    This reverts commit 695b960fd29bb36d6b385b83b7538ad9dc86e6bc.
    
    Because it's a useful default obviously :P

--- nvim/lua/general/init.lua
+++ nvim/lua/general/init.lua
@@ -8,6 +8,11 @@ vim.opt.cursorline = false
 vim.opt.number = true
 vim.opt.relativenumber = false
 
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.expandtab = true
+
 if vim.g.vscode ~= nil then
   return
 end
